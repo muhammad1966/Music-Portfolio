e9da347e74e7f7f63bee9c80f181e608cb73adc1
Muhammad <bsmuhammad352@gmail.com>
2024-01-09 00:33:37 +0100

    mp5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Hero from './hero/Hero';
 
 function App() {
   return (
     <div className="App">
-      
+      <Hero />
     </div>
   );
 }
--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './hero.css';
+import { IoMdMusicalNote } from "react-icons/io";
+import logo from '../assets/logo.png'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className='logo-div'>
+        <img src={logo} alt="logo" />
+        <h3>Ella</h3>
+      </div>
+      <div className='deco-1'></div>
+      <IoMdMusicalNote className='music-note' size={120} color='#D8BCAB' />
+    </div>
   )
 }
 

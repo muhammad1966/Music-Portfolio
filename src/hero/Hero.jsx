4061502784a74c37c4e8530f1744ca52acb9b131
Muhammad <bsmuhammad352@gmail.com>
2024-01-09 21:11:36 +0100

    mp8

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
     <div className='hero'>
       <div className='logo-div'>
         <img src={logo} alt="logo" />
-        <h3>Ella</h3>
+        <h3>Eli</h3>
       </div>
       <div className='deco-1'>
       <span className='kas'></span>
@@ -26,6 +26,7 @@ const Hero = () => {
       <div className='content'>
         <div className='content-text'>
             <h1>Hi there!</h1>
+            <h1>I am Elizabeth</h1>
         </div>
 
         <div className='image-div'>

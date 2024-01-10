eb07019fd7eaa64e5e5a8dc2c7587d79aa6cedf3
Muhammad <bsmuhammad352@gmail.com>
2024-01-10 22:54:51 +0100

    mp7

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -3,6 +3,7 @@ import './hero.css';
 import { IoMdMusicalNote } from "react-icons/io";
 import { IoMusicalNote } from "react-icons/io5";
 import { FaMusic } from "react-icons/fa6";
+import { MdOutlineMusicNote } from "react-icons/md";
 import HeroP from '../assets/Original1.png'
 
 
@@ -23,6 +24,7 @@ const Hero = () => {
       <IoMdMusicalNote className='music-note1' size={70} color='#D8BCAB' />
       <IoMusicalNote className='music-note2' size={40} color='#D8BCAB' />
       <FaMusic className='music-note3' size={30} color='#D8BCAB' />
+      <MdOutlineMusicNote className='music-note4' size={20} color='#D8BCAB' />
       <div className='content'>
         <div className='content-text'>
             <h1>Hi there!</h1>

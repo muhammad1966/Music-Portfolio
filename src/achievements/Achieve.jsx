4f13401afb734ca42738b9525d8a52426a13b7ec
Muhammad <bsmuhammad352@gmail.com>
2024-01-15 22:05:07 +0100

    15/1/2024

---
+++ src/achievements/Achieve.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './achieve.css'
+
+const Achieve = () => {
+  return (
+    <div className='acheive'>
+		<div>
+			<div></div>
+			<div></div>
+		</div>
+		<div>
+			<div></div>
+			<div></div>
+		</div>
+    </div>
+  )
+}
+
+export default Achieve

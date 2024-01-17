42933290da76d3aafb20d27e51c46e0e2724a3fd
Muhammad <bsmuhammad352@gmail.com>
2024-01-17 21:11:37 +0100

    17/1/2024

--- src/achievements/Achieve.jsx
+++ src/achievements/Achieve.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './achieve.css'
+import bl from '../assets/image2.png'
 
 const Achieve = () => {
   return (
     <div className='acheive'>
 		<div className='acheiv1'>
-			<div></div>
+			<div>
+				<img src={bl} alt="bl" />
+			</div>
 			<div></div>
 		</div>
 		<div>

958afa4fa6ab4e1e5530d93bbd2669b5fd291619
Muhammad <bsmuhammad352@gmail.com>
2024-01-20 21:21:43 +0100

    20/1/2024

--- src/achievements/Achieve.jsx
+++ src/achievements/Achieve.jsx
@@ -13,6 +13,9 @@ const Achieve = () => {
 				<h1>
 					MUSIC IS LIFE
 				</h1>
+				<h3>
+				A universal language that transcends boundaries and speaks to the depths of our souls. It weaves emotions into melodies, turning moments into memories and echoing the rhythm of our journey. Whether joyous or melancholic, music is the heartbeat of our existence, orchestrating the symphony of our shared human experience.
+				</h3>
 			</div>
 		</div>
 		<div>

9f1e79ef2b3d73ffd021833cee816a22361abe99
Muhammad <bsmuhammad352@gmail.com>
2024-01-25 23:21:52 +0100

    25/1/2024

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
         </div>
 
         <div className='image-div'>
-            <img src={HeroP} alt="kip" />
+            <img src={HeroP} alt="" />
         </div>
       </div>
       
